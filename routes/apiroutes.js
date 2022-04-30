6d3d4c2f8fe5b0b8205e71aa5e5d9a091b6adc7f
Ricky Thakar <rickythakar@gmail.com>
2022-04-30 13:49:52 -0400

    updated apiRoutes file and deleted unused code

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,18 +1,11 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
-const dbJSON = require('../db/db.json');
-// console.log(dbJSON);
 
-//Require the path and router items needed
-const path = require('path');
-const { json } = require('express/lib/response');
 
 // All routes are prefixed with /api - ref ln18 sever.js file
 router.get('/notes', (req,res) => {
-    readFromFile('./db/db.json').then((data)=> res.json(json.parse(data)));
-    console.log('api/notes')
-    res.json(dbJSON)
+    readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)));
 });
 router.get('/notes:id', (req,res) => {
     readFromFile('/db/db.json')
@@ -28,7 +21,7 @@ router.get('/notes:id', (req,res) => {
 router.post('/notes', (req,res) => {
     console.log(req.body)
 
-    const {title, text, id} = req.body;
+    const {title, text} = req.body;
     const newNote = {
         title,
         text,
@@ -37,8 +30,7 @@ router.post('/notes', (req,res) => {
     if(req.body){
         readAndAppend(newNote,'./db/db.JSON');
         res.json('note created');
-    }
-    else {
+    } else {
         res.error('note create failed')
     }
 });
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,19 +1,16 @@
 //Require the path and router items needed
-const req = require('express/lib/request');
-const res = require('express/lib/response');
+// const req = require('express/lib/request');
+// const res = require('express/lib/response');
 const path = require('path');
 const router = require('express').Router();
 
 //Need to add the router.get functionality for each of the html pages
 router.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
-//function not finished insert directory name
 });
 
-
 router.get('*', (req,res) => {
-res.sendFile(path.join("../public/index.html"));
-//insert path name into function
+res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Export the routes
