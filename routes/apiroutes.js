d8feecbfffaf7ac0f393fac2f437ebdde61e45e5
Ricky Thakar <rickythakar@gmail.com>
2022-04-30 12:35:03 -0400

    constructed api routes file and functions. Tested api routes on insonmia. Hookedup dbjson to api routes.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,23 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const dbJSON = require('../db/db.json');
+console.log(dbJSON);
 
+//Require the path and router items needed
+const path = require('path');
+
+//Need to add the router.get functionality for each of the html pages
+// All routes are prefixed with /api - ref ln18 sever.js file
+router.get('/notes', (req,res) => {
+    console.log('api/notes')
+    res.json(dbJSON)
+});
+
+router.post('/notes', (req,res) => {
+    
+});
+
+
+//exports the routes
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 
 //Dependencies 
 const express = require('express');
-const { append } = require('express/lib/response');
 //Required Routes
-const apiRoutes = require("./routes/apiroutes")
-const htmlRoutes = require("./routes/htmlroutes")
+const apiRoutes = require("./routes/apiRoutes");
+// const {use} = require('./routes/htmlRoutes');
+const htmlRoutes = require("./routes/htmlRoutes")
 
 
 //Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
+// Route the Middleware
 app.use(express.json());
-
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use('/api',apiRoutes);
+app.use('/',htmlRoutes);
 
 
 // Start the server
-append.listen(PORT, () => console.log('This port is listening at ${PORT}'));
+app.listen(PORT, () => console.log(`This port is listening at ${PORT}`));
