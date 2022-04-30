0022885d289f347c088a1163fee4faefc829e75f
Ricky Thakar <rickythakar@gmail.com>
2022-04-30 13:17:03 -0400

    push to heroku

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const dbJSON = require('../db/db.json');
-console.log(dbJSON);
+// console.log(dbJSON);
 
 //Require the path and router items needed
 const path = require('path');
@@ -15,9 +15,21 @@ router.get('/notes', (req,res) => {
 });
 
 router.post('/notes', (req,res) => {
-    
+    console.log(req.body)
+
+    const {title, text, id} = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uuidv4()
+    };
+    if(req.body){
+        readAndAppend(newNote,'./db/db.JSON');
+        res.json('note created');
+    }
+    else {
+        res.error('note create failed')
+    }
 });
-
-
 //exports the routes
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
 //Dependencies 
 const express = require('express');
 //Required Routes
 const apiRoutes = require("./routes/apiRoutes");
 // const {use} = require('./routes/htmlRoutes');
 const htmlRoutes = require("./routes/htmlRoutes")
-
 
 //Initialize the app and create a port
 const app = express();
@@ -17,7 +15,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use('/api',apiRoutes);
 app.use('/',htmlRoutes);
-
 
 // Start the server
 app.listen(PORT, () => console.log(`This port is listening at ${PORT}`));
