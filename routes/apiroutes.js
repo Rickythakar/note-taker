ff47ca49c229f67be08d8fe3d88d375ccc09099d
Ricky Thakar <rickythakar@gmail.com>
2022-04-30 00:07:03 -0400

    declared dependecies and created route files

---
+++ routes/apiroutes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
---
+++ server.js
@@ -0,0 +1,7 @@
+
+//Dependencies 
+const express = require('express');
+const apiRoutes = require("./routes/apiroutes")
+const htmlRoutes = require("./routes/htmlroutes")
+
+
